3bccd23e6cd5d7633be8167101b76ffc73413ad0
Razaul Munir <razaulmunir@gmail.com>
2019-12-13 12:57:01 +0600

    write fuction to count with onClick

--- src/Practice.js
+++ src/Practice.js
@@ -1,1 +1,18 @@
 import React, {useState} from 'react';
+
+const Practice=(props)=>{
+    return(
+        <div>
+            <p>{props.count}</p>
+        
+
+            <button onClic={()=>
+                {
+                    props.setCounter(props.count+1)
+                }
+            }>Add</button>
+        </div>
+    );
+};
+
+export default Practice;
