71a5add329b237e6a4486c93257fa4da4bc10d35
Razaul Munir <razaulmunir@gmail.com>
2019-12-14 10:40:40 +0600

    add more color change button

--- src/Practice.js
+++ src/Practice.js
@@ -15,6 +15,15 @@ const Practice=(props)=>{
                     else
                         document.body.style='background: green;';
             }}>Add</button>
+
+            <button onClick={()=>{
+                props.setCount(props.count+1);
+                    document.body.style = 'background: blue;';
+            }}>Blue</button>
+            <button onClick={()=>{
+                props.setCount(props.count+1);
+                    document.body.style = 'background: black;';
+            }}>Black</button>
         </div>
     );
 };
